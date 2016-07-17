5f2396b79ec004f8780ab0b737e66d8394236528
Stanislav Gumeniuk <i@vigo.su>
2016-07-17 20:19:39 +0300

    [fix] tests

--- tests/miniheap.js
+++ tests/miniheap.js
@@ -3,21 +3,21 @@
  */
 
 var assert = require('chai').assert;
-let Heap = require('../src/');
+var Heap = require('../src/');
 describe('services/cache', function () {
 
 
     describe('Check sort', function () {
         it('empty array', function () {
-            let heap = new Heap([]);
+            var heap = new Heap([]);
             assert.sameMembers(heap.data, []);
         });
 
-        for (let i=1;i<=4;i++){
+        for (var i=1;i<=4;i++){
             it('test '+i, function () {
-                let inData = require('../tests_mocks/sort/'+i+'/in.js');
-                let outData = require('../tests_mocks/sort/'+i+'/out.js');
-                let heap = new Heap(inData);
+                var inData = require('../tests_mocks/sort/'+i+'/in.js');
+                var outData = require('../tests_mocks/sort/'+i+'/out.js');
+                var heap = new Heap(inData);
                 assert.deepEqual(heap.data, outData);
             });
         }
@@ -25,15 +25,15 @@ describe('services/cache', function () {
 
     describe('Check heapify', function () {
         it('empty array', function () {
-            let heap = new Heap([]);
+            var heap = new Heap([]);
             assert.sameMembers(heap.heapify(), []);
         });
 
-        for (let i=1;i<=6;i++){
+        for (var i=1;i<=6;i++){
             it('test '+i, function () {
-                let inData = require('../tests_mocks/heapify/'+i+'/in.js');
-                let outData = require('../tests_mocks/heapify/'+i+'/out.js');
-                let heap = new Heap(inData);
+                var inData = require('../tests_mocks/heapify/'+i+'/in.js');
+                var outData = require('../tests_mocks/heapify/'+i+'/out.js');
+                var heap = new Heap(inData);
                 console.log(heap.heapify());
                 assert.deepEqual(heap.heapify(), outData);
             });
